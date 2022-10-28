6bb9917a556072223ef8e01c29c58f5952e2f876
lissavzx <114089485+lissavzx@users.noreply.github.com>
2022-10-28 12:58:00 +0300

    Add files via upload

---
+++ lesson_2/task 5.py
@@ -0,0 +1,10 @@
+number = int(input("enter your number"))
+sum=0
+for i in range (number):
+    x = int(input("enter your number"))
+    sum += x
+print(sum)
+m = (max (number, x))
+print(m)
+mi = (min (number, x))
+print(mi)
